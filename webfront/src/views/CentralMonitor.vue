23cfe5cc3f6f3de376bbf813a9d18fd22f00c5d4
R52125 <r714852751@163.com>
2021-06-01 10:27:49 +0800

    webfront完成99％，userweb发送未完成

--- webfront/src/views/CentralMonitor.vue
+++ webfront/src/views/CentralMonitor.vue
@@ -1,38 +1,38 @@
 <template>
 <div>
-  <div class="card shadow-lg ml-5 mr-5">
+  <div class="card shadow-lg ml-5 mr-5 mt-3 mb-3" v-for="data in monitorlist">
     <div class="card-body">
       <div>
         <br>
-        <h3>房间号：{{ Room_id }}</h3>
+        <h3>房间号：{{ data.Room_id }}</h3>
 <!--        <div>{{cstate}}</div>-->
 <!--        <div>{{Rmode}}</div>-->
 <!--        <div>{{tem}}</div>-->
 <!--        <div>{{wind}}</div>-->
-        <div v-if="cstate == 1">
+        <div v-if="data.cstate == 1">
           <br>
           <h5>空调开机状态：已开机</h5>
           <br>
-          <div v-if="Rmode == 0">
+          <div v-if="data.Rmode == 0">
             <h5>空调模式：制冷</h5>
           </div>
           <div v-else>
             <h5>空调模式：供暖</h5>
           </div>
           <br>
-          <h5>房间温度：{{ tem }}℃</h5>
+          <h5>房间温度：{{ data.tem }}℃</h5>
           <br>
-          <div v-if="wind == 0">
+          <div v-if="data.wind == 0">
             <h5>空调风速：低</h5>
           </div>
-          <div v-else-if="wind == 1">
+          <div v-else-if="data.wind == 1">
             <h5>空调风速：中</h5>
           </div>
-          <div v-else="wind == 2">
+          <div v-else="data.wind == 2">
             <h5>空调风速：高</h5>
           </div>
         </div>
-        <div v-if="cstate == 0">
+        <div v-if="data.cstate == 0">
           <br>
           <h5>空调开机状态：未开机</h5>
         </div>
@@ -48,7 +48,7 @@ import {mapState} from "vuex";
 export default {
   name: "CentralMonitor",
   computed:{
-    ...mapState(['Room_id', 'cstate', 'Rmode', 'tem', 'wind']),
+    ...mapState(['Room_id', 'monitorlist']),
   }
 }
 </script>
--- webfront/src/views/Formmes.vue
+++ webfront/src/views/Formmes.vue
@@ -4,10 +4,10 @@
 
     <div class="card mr-5 ml-5 shadow-lg mb-5">
       <div class="card-header text-center">
-        <h4>房间号：1</h4>
+        <h4>房间号：{{ Room_id }}</h4>
       </div>
       <div class="card-body mr-5 ml-5">
-        <p class="card-text">从控机开关机次数：5</p>
+        <p class="card-text">从控机开关机次数：{{ up_times }}</p>
         <div class="table-responsive pagination">
           <table class="table table-striped">
             <thead>
@@ -22,86 +22,80 @@
             </tr>
             </thead>
             <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>2021.05.20 9:20</td>
-              <td>2021.05.20 9:21</td>
-              <td>23℃</td>
-              <td>25℃</td>
-              <td>100L</td>
-              <td>￥100</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
+            <tr v-for="data in formdatalist" :key="data.id">
+              <th scope="row">{{ data.id }}</th>
+              <td>{{ data.start_time }}</td>
+              <td>{{ data.stop_time }}</td>
+              <td>{{ data.start_temp }}℃</td>
+              <td>{{ data.end_temp }}℃</td>
+              <td>{{ data.wind_power }}L</td>
+              <td>￥{{ data.cost }}</td>
             </tr>
             </tbody>
           </table>
         </div>
+        <p>每日所需总费用：{{total_cost}}</p>
       </div>
     </div>
 
-    <div class="card mr-5 ml-5 shadow-lg">
-      <div class="card-header text-center">
-        <h4>房间号：2</h4>
-      </div>
-      <div class="card-body mr-5 ml-5">
-        <p class="card-text">从控机开关机次数：5</p>
-        <div class="table-responsive pagination">
-          <table class="table table-striped">
-            <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">开启时间</th>
-              <th scope="col">关闭时间</th>
-              <th scope="col">初始温度</th>
-              <th scope="col">目标温度</th>
-              <th scope="col">风量消耗</th>
-              <th scope="col">所需费用</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>2021.05.20 9:20</td>
-              <td>2021.05.20 9:21</td>
-              <td>23℃</td>
-              <td>25℃</td>
-              <td>100L</td>
-              <td>￥100</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+<!--    <div class="card mr-5 ml-5 shadow-lg">-->
+<!--      <div class="card-header text-center">-->
+<!--        <h4>房间号：2</h4>-->
+<!--      </div>-->
+<!--      <div class="card-body mr-5 ml-5">-->
+<!--        <p class="card-text">从控机开关机次数：5</p>-->
+<!--        <div class="table-responsive pagination">-->
+<!--          <table class="table table-striped">-->
+<!--            <thead>-->
+<!--            <tr>-->
+<!--              <th scope="col">#</th>-->
+<!--              <th scope="col">开启时间</th>-->
+<!--              <th scope="col">关闭时间</th>-->
+<!--              <th scope="col">初始温度</th>-->
+<!--              <th scope="col">目标温度</th>-->
+<!--              <th scope="col">风量消耗</th>-->
+<!--              <th scope="col">所需费用</th>-->
+<!--            </tr>-->
+<!--            </thead>-->
+<!--            <tbody>-->
+<!--            <tr>-->
+<!--              <th scope="row">1</th>-->
+<!--              <td>2021.05.20 9:20</td>-->
+<!--              <td>2021.05.20 9:21</td>-->
+<!--              <td>23℃</td>-->
+<!--              <td>25℃</td>-->
+<!--              <td>100L</td>-->
+<!--              <td>￥100</td>-->
+<!--            </tr>-->
+<!--            <tr>-->
+<!--              <th scope="row">2</th>-->
+<!--              <td>Jacob</td>-->
+<!--              <td>Thornton</td>-->
+<!--              <td>@fat</td>-->
+<!--            </tr>-->
+<!--            <tr>-->
+<!--              <th scope="row">3</th>-->
+<!--              <td>Larry</td>-->
+<!--              <td>the Bird</td>-->
+<!--              <td>@twitter</td>-->
+<!--            </tr>-->
+<!--            </tbody>-->
+<!--          </table>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
 
   </div>
 </template>
 
 <script>
+import {mapState} from "vuex";
+
 export default {
-  name: "Formmes"
+  name: "Formmes",
+  computed:{
+    ...mapState(['Room_id', 'up_times', 'total_cost', 'formdatalist']),
+  }
 }
 </script>
 
